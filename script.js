69f2c5763b9bc222d717c363baa1c208ee1bb6b4
Emir <emir.ulusoy@student.forsbergsskola.se>
2025-04-03 21:06:46 +0200

    wrote image display function

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function showImage()
+{
+    const weatherIcon = document.getElementById('weather-icon');
+    weatherIcon.style.display = 'block';
+}
+
 function displayHourlyForecast(hourlyData)
 {
     const hourlyForecastDiv = document.getElementById('hourly-forecast');
